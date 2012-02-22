b1ed2120be7b4147f2a91ad3c74650aa0a532063
Jacek Karolak <j.karolak@sabr.pl>
2012-02-22 23:06:05 +0100

    Added dis, pprint to __builtins__ for performance

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,3 +1,17 @@
+from dis import dis
+from pprint import pprint
+__builtins__.dis = dis
+__builtins__.pprint = pprint
+del dis, pprint
+
+try:
+    import readline
+except ImportError:
+    print("Module readline not available.")
+else:
+    import rlcompleter
+    readline.parse_and_bind("tab: complete")
+
 try:
     from see import see
 except ImportError:
